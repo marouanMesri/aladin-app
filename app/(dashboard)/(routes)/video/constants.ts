e4e35a20088a6b3aec43569976de29ced440a160
user <marouan31@gmail.com>
2023-11-04 17:34:11 +0100

    add db

---
+++ app/(dashboard)/(routes)/video/constants.ts
@@ -0,0 +1,4 @@
+import * as z from "zod";
+export const formSchema = z.object({
+  prompt: z.string().min(1, { message: "Video prompt is required" }),
+});
---
+++ lib/api-limit.ts
@@ -0,0 +1,5 @@
+import { auth } from "@clerk/nextjs";
+
+import prismadb from "./prismadb";
+
+import { MAX_FREE_COUNTS } from "@/constants";
